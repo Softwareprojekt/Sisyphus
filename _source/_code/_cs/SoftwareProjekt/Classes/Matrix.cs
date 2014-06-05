886017560bba21ab3848c4a133ae94c583fd63c5
Lennok <marcel.waida@gmail.com>
2014-06-05 02:47:42 +0200

    Added Matrix implementation

--- _source/_code/_cs/SoftwareProjekt/Classes/Matrix.cs
+++ _source/_code/_cs/SoftwareProjekt/Classes/Matrix.cs
@@ -33,17 +33,20 @@ namespace SoftwareProjekt
         /// </summary>
         public Matrix(float x_11, float x_12, float x_21, float x_22)
         {
-            throw new System.NotImplementedException();
+            X11 = x_11;
+            X12 = x_12;
+            X21 = x_21;
+            X22 = x_22;
         }
 
-        public float Determinante
+        /// <summary>
+        /// Gets the Determinant of this Matrix
+        /// </summary>
+        public float Determinant
         {
             get
             {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
+                return X11 * X22 - X12 * X21;
             }
         }
 
@@ -60,19 +63,113 @@ namespace SoftwareProjekt
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Inverts this Matrix
+        /// </summary>
         public void Invert()
         {
-            throw new System.NotImplementedException();
+            Matrix m = (Matrix) this.MemberwiseClone();
+            float tmp = X11;
+            this.X11 = X22;
+            this.X22 = tmp;
+            this.X12 *= -1;
+            this.X21 *= -1;
+            if (this.Determinant != 0f)
+            {
+                this.Multiply(1 / Determinant);
+            }
+            else
+            {
+                 this.X11 = m.X11;
+                 this.X22  = m.X22;
+                 this.X12 = m.X12;
+                 this.X21 = m.X21;
+                 throw new ArgumentException("The Matrix values are not vaild for this operation, determinant = 0");
+            }
+            
         }
 
+        /// <summary>
+        /// Inverts a Matrix
+        /// </summary>
+        static public Matrix Invert(Matrix matrix)
+        {
+            if (matrix.Determinant != 0f)
+            {
+                Matrix m = new Matrix(matrix.X22, -matrix.X12, -matrix.X21, matrix.X11);
+                m.Multiply(1 / matrix.Determinant);
+                return m;
+            }
+            else
+            {
+                throw new ArgumentException("The Matrix values are not vaild for this operation, determinant = 0");
+            }
+        }
+        /// <summary>
+        /// Multiplies this Matrix with a scalar
+        /// </summary>
+        public void Multiply(float scalar)
+        {
+            X11 *= scalar;
+            X12 *= scalar;
+            X21 *= scalar;
+            X22 *= scalar;
+        }
+        /// <summary>
+        /// Multiplies a Matrix with an scalar and retruns the new Matrix
+        /// </summary>
+        static public Matrix Multiply(Matrix matrix, float scalar)
+        {
+            Matrix m = new Matrix(matrix.X11, matrix.X12, matrix.X21, matrix.X22);
+            m.Multiply(scalar);
+            return m;
+        }
+
+
+        /// <summary>
+        /// Multiplies this Matrix with an other one
+        /// </summary>
         public void Multiply(Matrix newMatrix)
         {
-            throw new System.NotImplementedException();
+            float x_11 = X11 * newMatrix.X11 + X12 * newMatrix.X21;
+            float x_12 = X11 * newMatrix.X21 + X12 * newMatrix.X22;
+            float x_21 = X21 * newMatrix.X11 + X22 * newMatrix.X21;
+            float x_22 = X21 * newMatrix.X12 + X22 * newMatrix.X22;
+            this.X11 = x_11;
+            this.X12 = x_12;
+            this.X21 = x_21;
+            this.X22 = x_22;
         }
 
+        /// <summary>
+        /// Multiplies two Matrices and retruns a new Matrix
+        /// </summary>
         public Matrix Multiply(Matrix firstMatrix, Matrix secondMatrix)
         {
-            throw new System.NotImplementedException();
+            float x_11 = firstMatrix.X11 * secondMatrix.X11 + firstMatrix.X12 * secondMatrix.X21;
+            float x_12 = firstMatrix.X11 * secondMatrix.X21 + firstMatrix.X12 * secondMatrix.X22;
+            float x_21 = firstMatrix.X21 * secondMatrix.X11 + firstMatrix.X22 * secondMatrix.X21;
+            float x_22 = firstMatrix.X21 * secondMatrix.X12 + firstMatrix.X22 * secondMatrix.X22;
+            return new Matrix(x_11, x_12, x_21, x_22);
+        }
+
+        /// <summary>
+        /// Adds a Matrix to this Matrix
+        /// </summary>
+        public void Add(Matrix newMatrix)
+        {
+            this.X11 += newMatrix.X11;
+            this.X12 += newMatrix.X12;
+            this.X21 += newMatrix.X21;
+            this.X22 += newMatrix.X22;
+        }
+
+        /// <summary>
+        /// Adds two Matrices and returns a new one
+        /// </summary>
+        static public Matrix Add(Matrix firstMatrix, Matrix secondMatrix)
+        {
+            return new Matrix(firstMatrix.X11 + secondMatrix.X11, firstMatrix.X12 + secondMatrix.X12, firstMatrix.X21 + secondMatrix.X21, firstMatrix.X22 + secondMatrix.X22);
         }
     }
 }
