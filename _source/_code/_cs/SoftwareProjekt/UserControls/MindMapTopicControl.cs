828bf4cd02f99cf33bc3c9cbe796ac9e65c6f76d
tschar <julian@rost.biz>
2014-06-05 23:18:34 +0200

    repaired all the class diagrams
    also changed XmlTopicConnect to TopicConnect

--- _source/_code/_cs/SoftwareProjekt/UserControls/MindMapTopicControl.cs
+++ _source/_code/_cs/SoftwareProjekt/UserControls/MindMapTopicControl.cs
@@ -21,6 +21,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -101,7 +102,10 @@ namespace SoftwareProjekt
 
         void MindMapTopicControl_Paint(object sender, PaintEventArgs e)
         {
-            
+            Graphics g = e.Graphics;
+
+            g.DrawEllipse(Pens.Black, 0, 0, this.Width, this.Height);
+           // g.DrawString(this._
         }
     }
 }
