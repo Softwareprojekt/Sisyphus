b7c5ffae08d4c31547a9da0ca4f060cb0746f980
tschar <julian@rost.biz>
2014-06-08 23:11:08 +0200

    added example document for topics, also developed a xml parser and implemented all the good stuff for the mindmap

--- _source/_code/_cs/SoftwareProjekt/Classes/XmlTopic.cs
+++ _source/_code/_cs/SoftwareProjekt/Classes/XmlTopic.cs
@@ -8,30 +8,58 @@ namespace SoftwareProjekt
     public class XmlTopic
     {
         private string _topicName;
-        private int _xmlExerciseList;
+        private string _dllPath;
+
+        private List<XmlExercise> _xmlExerciseList;
         private int _topicID;
         private List<TopicConnect> _connectionList;
 
-        public int ExerciseList
+
+
+        public XmlTopic()
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+            _connectionList = new List<TopicConnect>();
+            _xmlExerciseList = new List<XmlExercise>();
+            _topicName = "";
+            _topicID = -1;
         }
 
-        public int TopicName
+        public List<XmlExercise> ExerciseList
         {
             get
             {
-                throw new System.NotImplementedException();
+                return _xmlExerciseList;
             }
             set
             {
+                _xmlExerciseList = value;
             }
+        }
+        public List<TopicConnect> ConnectionList
+        {
+            get { return _connectionList; }
+            set { _connectionList = value; }
+        }
+        public string TopicName
+        {
+            get
+            {
+                return _topicName;
+            }
+            set
+            {
+                _topicName = value;
+            }
+        }
+        public string DllPath
+        {
+            get { return _dllPath; }
+            set { _dllPath = value; }
+        }
+        public int TopicID
+        {
+            get { return _topicID; }
+            set { _topicID = value; }
         }
     }
 }
