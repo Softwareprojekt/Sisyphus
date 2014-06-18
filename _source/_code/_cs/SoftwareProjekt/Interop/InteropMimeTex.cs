bc9958af7046e2b1f706a6c6073afd2695985f2f
tschar <julian@rost.biz>
2014-06-19 00:12:35 +0200

    added interop - operations for mimetex.dll
    To actually use it, add the mimetex.dll to the path of the executable.

---
+++ _source/_code/_cs/SoftwareProjekt/Interop/InteropMimeTex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftwareProjekt.Interop
+{
+    [System.Security.SuppressUnmanagedCodeSecurity()]
+    class InteropMimeTex
+    {
+        private InteropMimeTex () // need this dummy
+        {
+        }
+
+        [System.Runtime.InteropServices.DllImport("MimeTeX.dll")]
+        internal static extern int CreateGifFromEq(string expr, string fileName);
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        internal extern static IntPtr GetModuleHandle(string lpModuleName);
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        internal extern static bool FreeLibrary(IntPtr hLibModule); 
+    }
+    
+}
