f417d64ad9811312964e0069996ae2f3b51e1ad1
Lennok <marcel.waida@gmail.com>
2014-06-20 16:32:30 +0200

    Added new Textbox which automatically parse the data and shows if the data is correct (red or white)

---
+++ _source/_code/_cs/SoftwareProjekt/Classes/EventArguments/ParseEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftwareProjekt.Classes.EventArguments
+{
+    class ParseEventArgs : EventArgs    
+    {
+        public ParseEventArgs(float value)
+        {
+            Value = value;
+        }
+        public float Value { get; set; }
+    }
+}
---
+++ _source/_code/_cs/SoftwareProjekt/UserControls/FloatInput.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SoftwareProjekt.UserControls
+{
+    class FloatInput : TextBox
+    {
+        
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            float f;
+            if (!this.Parse(this.Text.Replace(',', '.'), out f))
+            {
+                this.BackColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                this.BackColor = System.Drawing.Color.White;
+            }            
+            base.OnTextChanged(new Classes.EventArguments.ParseEventArgs(f));
+        }
+
+        private bool Parse(string s, out float f)
+        {
+            float retval = 0f;
+
+            if (float.TryParse(s, out retval))
+            {
+                f = retval;
+#if DEBUG
+                Console.WriteLine("SUCCESS @ FloatInput.Parse => " + f);
+#endif
+                return true;
+            }
+
+            f = float.NaN;
+#if DEBUG
+            Console.WriteLine("ERROR @ FloatInput.Parse => " + f);
+#endif
+            return false;
+        }
+    }
+}
